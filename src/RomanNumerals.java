122465d7542be9ffc583cb10560050d33c8daa03
rijuntun <rijuntun@STUPID-PC410.stnet.local>
2016-09-28 16:48:20 +0300

    working on it

--- src/RomanNumerals.java
+++ src/RomanNumerals.java
@@ -2,7 +2,21 @@
 public class RomanNumerals {
 	public int convertToInteger(String romanNum) {
 		// To be Implemented
-		return 0;
+		int retval=0;
+
+	    
+		
+		for (int i = 0, n = romanNum.length(); i < n; i++) {
+		    char c = romanNum.charAt(i);
+		    
+		    if (c=='I') 
+		    {
+		    	retval++;
+		    }	
+		 }
+	   
+		return retval;
+		
 		
 	}
 }
--- tests/TestRomanNumerals.java
+++ tests/TestRomanNumerals.java
@@ -3,10 +3,50 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class TestRomanNumerals {
+    RomanNumerals romans = new RomanNumerals();
+    
+	@Test
+	public void testRomanNumeralsCreated() {
+		//fail("Not yet implemented");
+		assertNotNull(romans);
+
+	}
+	@Test
+	public void test_I_one(){
+		
+		int num=romans.convertToInteger("I");
+		assertEquals(1,num);
+		
+	}
+	
+	@Test
+	public void testII_two(){
+		
+		int num=romans.convertToInteger("II");
+		assertEquals(2,num);
+		
+	}
+
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testIII_three(){
+		
+		int num=romans.convertToInteger("III");
+		assertEquals(3,num);
+		
+	}
+	@Test
+	public void testIV_four(){
+		int num=romans.convertToInteger("IV");
+		assertEquals(4, num);
+		
+	}
+
+	@Test
+	public void testV_five(){
+		int num=romans.convertToInteger("V");
+		assertEquals(5, num);
+		
 	}
 
 }
