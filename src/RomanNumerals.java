17a7883fb5eabba4e6f174dd69a42becf6e8e646
somename <someemail>
2016-10-05 16:35:36 +0300

    besouro automatic message

--- src/RomanNumerals.java
+++ src/RomanNumerals.java
@@ -6,44 +6,50 @@ public class RomanNumerals {
 		int retval=0;
 		int lastNumber=0;
 		int lastRmToInt=0;
+		int next=0;
 
 	    
 		
 		for (int i = 0, n = romanNum.length(); i < n; i++) {
 		    char c = romanNum.charAt(i);
+		    if (romanNum.length()<i)
+		    	next=romanToInt(romanNum.charAt(i+1));
+		    	
+		    else
+		    	next=0;
 		    
 		    switch  (c){
 		    
 		    case 'M':
 		    {
-		    	retval=processInt(1000,lastNumber,retval);
+		    	retval=processInt(1000,lastNumber,retval,next);
 		    	lastNumber=1000;
 		    }
 		    break;
 
 		    case 'D':
 		    {
-		    	retval=processInt(500,lastNumber,retval);
+		    	retval=processInt(500,lastNumber,retval,next);
 		    	lastNumber=500;
 		    }
 		    break;
 		    
 		    case 'C':
 		    {
-		    	retval=processInt(100,lastNumber,retval);
+		    	retval=processInt(100,lastNumber,retval, next);
 		    	lastNumber=100;
 		    }
 		    break;
 		    
 		    case 'L':
 		    {
-			    retval=processInt(50,lastNumber,retval);
+			    retval=processInt(50,lastNumber,retval, next);
 			    lastNumber=50;
 		    }
 		    break;
 		    case 'X':
 		    {
-			    retval=processInt(10,lastNumber,retval);
+			    retval=processInt(10,lastNumber,retval,next);
 			    lastNumber=10;
 		    }
 		    break;
@@ -52,14 +58,14 @@ public class RomanNumerals {
 		    case'V':
 		    {
 		    	
-		    	retval=processInt(5,lastNumber,retval);
+		    	retval=processInt(5,lastNumber,retval,next);
 		    	lastNumber=5;
 		    	
 		    }
 		    break;
 		    case 'I':
 		    {
-		    	retval=processInt(1,lastNumber,retval);
+		    	retval=processInt(1,lastNumber,retval,next);
 		    	lastNumber=1;
 		    }	
 		    break;
@@ -80,4 +86,19 @@ public class RomanNumerals {
 	            return cumulativeValue + rmToInt;
 	        }
 	    }
+	  
+	  public static int romanToInt(char c){
+		  switch (c){
+		  case 'M': return 1000;
+		  case 'D': return 500;
+		  case 'C': return 100;
+		  case 'L': return 50;
+		  case 'X': return 10;
+		  case 'V': return 5;
+		  case 'I': return 1;
+		  default: return 0;
+		  }
+	  }
+	  
+	  
 }
